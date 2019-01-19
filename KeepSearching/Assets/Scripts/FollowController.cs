dea68a1be254b908de1a13d7c5bfabc436bb3770
Dylan Dawkins <dad586@nyu.edu>
2019-01-19 14:01:35 -0500

    Add FollowController script

---
+++ KeepSearching/Assets/Scripts/FollowController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowController : MonoBehaviour
+{
+    private float speed = 100.0f;
+    public Rigidbody rb;
+    public Transform controller;
+    public Transform controllerBody;
+    Vector3 controllerPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        HoldObject();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        {
+            print("Points colliding: " + collision.contacts.Length);
+            print("First normal of the point that collide: " + collision.contacts[0].normal);
+        }
+    }
+    void HoldObject()
+    {
+        //Create Vector 3 for controller position that keeps object at certain distance from controller
+        controllerPosition = controller.position;
+        rb.AddForce((controllerPosition - transform.position) * speed);
+        transform.LookAt(controllerBody);
+
+    }
+}
