41f4a3915b57fd3e5b0de26ba1f4f08479e9a3b4
Andrés Ornelas <andresornelas@gmail.com>
2019-01-19 01:53:25 -0500

    Update scene seed with navigation controls
    > Improve headpose canvas text
    > Add controller tooltips
    > Add scene navigation logic

---
+++ KeepSearching/Assets/Scripts/SceneNavigatorController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.MagicLeap;
+
+[RequireComponent(typeof(ControllerConnectionHandler))]
+public class SceneNavigatorController : MonoBehaviour
+{
+
+    public string NextScene;
+    public string AlternativeScene;
+
+    private ControllerConnectionHandler _controllerConnectionHandler;
+
+    void Start () {
+        _controllerConnectionHandler = GetComponent<ControllerConnectionHandler>();
+        MLInput.OnTriggerDown += HandleOnTriggerDown;
+        MLInput.OnControllerButtonDown += HandleOnButtonDown;
+    }
+
+    /// <summary>
+    /// Handles the event for trigger down.
+    /// </summary>
+    /// <param name="controllerId">The id of the controller.</param>
+    /// <param name="value">The value of the trigger button.</param>
+    private void HandleOnTriggerDown(byte controllerId, float value)
+    {
+        MLInputController controller = _controllerConnectionHandler.ConnectedController;
+        if (controller != null && controller.Id == controllerId) {
+            SceneManager.LoadScene(NextScene);
+        }
+    }
+
+    /// <summary>
+    /// Handles the event for trigger down.
+    /// </summary>
+    /// <param name="controllerId">The id of the controller.</param>
+    /// <param name="button">The button that was pressed.</param>
+    private void HandleOnButtonDown(byte controllerId, MLInputControllerButton button)
+    {
+        MLInputController controller = _controllerConnectionHandler.ConnectedController;
+        if (controller != null && controller.Id == controllerId && button == MLInputControllerButton.Bumper) {
+            SceneManager.LoadScene(AlternativeScene);
+        }
+    }
+}
