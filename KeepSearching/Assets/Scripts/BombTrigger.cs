bebffb8246ab8b8ac4caf4a5dfd8a3c84a0d16df
Andrés Ornelas <andresornelas@gmail.com>
2019-01-20 11:18:39 -0500

    Make bomb explode when activated

---
+++ KeepSearching/Assets/Scripts/BombTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BombTrigger : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameStateManager.instance.currentGamePhase == GameStateManager.Phase.HIDING_CUTTERS) {
+            if (other.CompareTag("GameController")) {
+                other.GetComponent<ExplosionOne>().Explode();
+            }
+        }
+    }
+}
